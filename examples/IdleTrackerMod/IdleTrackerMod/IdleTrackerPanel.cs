09b1d4eb853b6c933b4fd0f47a48ae2e87eb32a8
minexew <minexew@gmail.com>
2017-12-28 17:34:38 +0100

    Add examples/IdleTrackerMod

---
+++ examples/IdleTrackerMod/IdleTrackerMod/IdleTrackerPanel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Game.GUI.Controls;
+using Microsoft.Xna.Framework;
+
+namespace Mod
+{
+    internal class IdleTrackerPanel : Panel
+    {
+        public IdleTrackerPanel(Manager manager) : base(manager)
+        {
+            this.Init();
+            this.Anchor = Anchors.Bottom | Anchors.Right;
+            this.Color = new Color(0.0f, 0.0f, 0.0f, 0.8f);
+
+            this.Height = 100;
+            this.Width = 100;
+        }
+
+        public Button AddButton(string text)
+        {
+            var button = new Game.GUI.Controls.Button(this.manager_0);
+            button.Init();
+            button.Width = 200;
+            button.Left = button.Margins.Left;
+            button.Top = this.Height + button.Margins.Top;
+            button.Text = text;
+            this.Add(button, true);
+
+            this.Width = Math.Max(this.Width, button.Margins.Left + button.Width + button.Margins.Right);
+            this.Height += button.Margins.Top + button.Height + button.Margins.Bottom;
+
+            return button;
+        }
+
+        public void AddLabel(string text)
+        {
+            var label = new Game.GUI.Controls.Label(this.manager_0);
+            label.Init();
+            label.Width = 200;
+            label.Left = label.Margins.Left;
+            label.Top = this.Height + label.Margins.Top;
+            label.Text = text;
+            label.Alignment = Alignment.TopRight;
+            this.Add(label, true);
+
+            this.Height += label.Margins.Top + label.Height + label.Margins.Bottom;
+        }
+
+        public void ClearContents()
+        {
+            this.ClientArea.controlsList_1.Clear();
+            this.Height = 0;
+        }
+    }
+}
---
+++ examples/IdleTrackerMod/IdleTrackerMod/ModMain.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+using ModLoader;
+using Game;
+using Game.GUI;
+
+namespace Mod
+{
+    public class ModMain : IMod
+    {
+        public static ModMain instance;
+
+        private const string name = "IdleTrackerMod";
+        private const float updatePeriod = 1.0f;
+        private GClass0 hudPanel;
+        private IdleTrackerPanel idleTrackerPanel;
+        private float timeSinceLastUpdate = 0.0f;
+
+        public ModMain()
+        {
+            instance = this;
+        }
+
+        public void OnLoad(HookManager hookManager)
+        {
+            hookManager.InGameHUDInit += HookManager_InGameHUDInit;
+            hookManager.UpdateInGame += HookManager_UpdateInGame;
+        }
+
+        private void HookManager_UpdateInGame(float realTimeDelta, float gameTimeDelta)
+        {
+            timeSinceLastUpdate += gameTimeDelta;
+
+            while (timeSinceLastUpdate > updatePeriod)
+            {
+                timeSinceLastUpdate -= updatePeriod;
+
+                Update();
+            }
+        }
+
+        private void HookManager_InGameHUDInit(Game.GUI.InGameHUD inGameHUD, Game.GUI.Controls.Manager manager)
+        {
+            this.hudPanel = inGameHUD.gclass0_0;
+
+            this.idleTrackerPanel = new IdleTrackerPanel(manager);
+            idleTrackerPanel.Visible = false;
+            hudPanel.Add(idleTrackerPanel);
+        }
+
+        private void Update()
+        {
+            Faction playerFaction = GnomanEmpire.Instance.World.AIDirector.PlayerFaction;
+
+            int numIdle = 0;
+
+            idleTrackerPanel.ClearContents();
+
+            foreach (KeyValuePair<uint, Character> gnome in playerFaction.Members)
+            {
+                //if (gnome.Value.Job == null && !gnome.Value.Body.IsSleeping)
+                if (gnome.Value.StatusText() == "Idle")
+                {
+                    numIdle++;
+
+                    var button = idleTrackerPanel.AddButton(gnome.Value.NameAndTitle());
+
+                    button.ToolTip.Text = gnome.Value.FormattedTopSkills(5);
+                    button.Click += (sender, e) => {
+                        GnomanEmpire.Instance.Camera.MoveTo(gnome.Value.Position, true, true);
+                    };
+                }
+            }
+
+            if (numIdle == 0)
+            {
+                idleTrackerPanel.Visible = false;
+                return;
+            }
+
+            idleTrackerPanel.AddLabel(numIdle + " idle gnomes");
+
+            idleTrackerPanel.SetPosition(
+                hudPanel.Width - idleTrackerPanel.Width - idleTrackerPanel.margins_0.Right,
+                hudPanel.Height - idleTrackerPanel.Height - idleTrackerPanel.margins_0.Bottom);
+
+            idleTrackerPanel.Visible = true;
+        }
+    }
+}
