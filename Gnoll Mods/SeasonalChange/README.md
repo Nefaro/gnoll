6e15ae6022dd9693a982cee289bef3852e74cf6f
Nefaro <xsvenson@gmail.com>
2021-08-31 00:39:00 +0300

    Mod: Seasonal change; change sprites depending on season

---
+++ Gnoll Mods/SeasonalChange/README.md
@@ -0,0 +1,4 @@
+This mod applies alternative sprite sheets on different seasons.
+The seasonal sprite sheets are provided under Data directory and 
+on season change the given named file is loaded, the textures are inserted 
+into the game and then reloaded
---
+++ Gnoll Mods/SeasonalChange/SeasonalChange/ModMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Game.GUI;
+using Game.GUI.Controls;
+using GnollModLoader;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GnollMods.SeasonalChange
+{
+    class ModMain : IGnollMod
+    {
+        private static readonly string assembly = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private static readonly string dll = Assembly.GetExecutingAssembly().GetName().Name;
+        private static readonly string dataFolder = assembly + "/" + dll + "/Data";
+
+        public string Name { get { return "SeasonalChange"; } }
+        public string Description { get { return "Changes the textures depending on the current season"; } }
+
+        public string BuiltWithLoaderVersion { get { return "G1.1"; } }
+
+        public void OnLoad(HookManager hookManager)
+        {
+            hookManager.InGameHUDInit += HookManager_InGameHudInit;
+        }
+
+        private void HookManager_InGameHudInit(InGameHUD inGameHUD, Manager manager)
+        {
+            // On first load, replace with current seasonal sprites
+            this.SwitchSpritesToSeason(Game.GnomanEmpire.Instance.Region.Season().ToString().ToLower());
+
+            Game.GnomanEmpire.Instance.Region.OnSeasonChange += OnSeasonChange;
+        }
+        private void OnSeasonChange(object sender, System.EventArgs e)
+        {
+            // good habbit to encapsulate your custom code
+            // so it wont crash the whole game
+            try
+            {
+                string season = Game.GnomanEmpire.Instance.Region.Season().ToString().ToLower();
+                System.Console.WriteLine("-- Season change event called: new season : " + season);
+                this.SwitchSpritesToSeason(season);
+                // Need to call the repack manually to replace the ingame sprites
+                this.CallTextureRepack();
+            }
+            catch (Exception)
+            {
+                // If we reach thisfar, then exception should not happen... but
+                // The game might be graphically in an undefined state, but not crashed
+                System.Console.WriteLine("-- Couldn't switch sprites ");
+            }
+        }
+
+        private void SwitchSpritesToSeason(string season)
+        {
+            string seasonalSprite = dataFolder + "/" + "default-" + season + ".png";
+            // Repopulate the "gamedefs -> sprite" map
+            Game.GnomanEmpire.Instance.gameDefs_0.method_12();
+
+            // In case our own spritesheet is missing,
+            // we can still reload everything
+            // That way the original spritesheets are loaded and everything is still fine
+            if (File.Exists(seasonalSprite))
+            {
+                System.Console.WriteLine("-- Switching season sprites to : " + season);
+
+                // Load our own custom spritesheet
+                Game.GnomanEmpire.Instance.gameDefs_0.method_81(seasonalSprite);
+
+                // Find all spritesheets that are "default.png" and replace them with our own
+                // Note: our do not end with "default.png" so we are safe from overwrite/rewrite
+                foreach (var key in Game.GnomanEmpire.Instance.gameDefs_0.dictionary_4.Keys.ToList())
+                {
+                    if (key.EndsWith("default.png"))
+                    {
+                        Game.GnomanEmpire.Instance.gameDefs_0.dictionary_4[key] =
+                            Game.GnomanEmpire.Instance.gameDefs_0.dictionary_4[seasonalSprite];
+
+                    }
+                }
+            }
+        }
+
+        private void CallTextureRepack()
+        {
+            // Do the internal book-keeping and texture-keeping
+            Game.GnomanEmpire.Instance.gameDefs_0.PackTextures();
+            Game.GnomanEmpire.Instance.world_0.Region.Map.TileSet.mTexture = Game.GnomanEmpire.Instance.gameDefs_0.Tilesheet;
+        }
+    }
+}
