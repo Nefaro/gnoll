c57b8b02f225345a6788f34f5ad2c60059704435
Nefaro <xsvenson@gmail.com>
2021-09-05 19:08:08 +0300

    Update mod to patch version 5

--- Gnoll Mods/FixStuckWheelbarrow/FixStuckWheelbarrow/ModMain.cs
+++ Gnoll Mods/FixStuckWheelbarrow/FixStuckWheelbarrow/ModMain.cs
@@ -13,29 +13,8 @@ namespace GnollMods.FixStuckWheelbarrow
     {
         public string Name { get { return "FixStuckWheelbarrow"; } }
         public string Description { get { return "Fixes a hauler being stuck on place with a full wheelbarrow"; } }
-
-        public string BuiltWithLoaderVersion { get { return "G1.3"; } }
-
-        public void OnLoad(HookManager hookManager)
-        {
-            hookManager.InGameHUDInit += HookManager_InGameHUDInit;
-        }
-
-        private void HookManager_InGameHUDInit(Game.GUI.InGameHUD inGameHUD, Game.GUI.Controls.Manager manager)
-        {
-            Faction playerFaction = GnomanEmpire.Instance.World.AIDirector.PlayerFaction;
-            // Iterate over all player entities (gnomes and automata nd whathaveyou)
-            foreach (KeyValuePair<uint, Character> entry in playerFaction.Members)
-            {
-                Character gnome = entry.Value;
-                // Interested only this specific Behavior
-                if ( gnome.Behavior.GetType() == typeof(PlayerCharacterBehavior) ||
-                    gnome.Behavior.GetType() == typeof(AutomatonBehavior))
-                {
-                    FindNextType((System.Collections.IEnumerable)gnome.Behavior, GetTargetTypesForPlayerCharacter().GetEnumerator());
-                }
-            }
-        }
+        public string BuiltWithLoaderVersion { get { return "G1.5"; } }
+        public int RequireMinPatchVersion { get { return 5; } }
 
         // Behavior "path" to where we need to end up
         private IEnumerable<Type> GetTargetTypesForPlayerCharacter()
@@ -47,6 +26,41 @@ namespace GnollMods.FixStuckWheelbarrow
             yield return typeof(GatherComponentsWithWheelBarrow);
             yield return typeof(GatherAllThenDropOffWB);
             yield return typeof(GetNextComponentWB);
+        }
+
+        public void OnLoad(HookManager hookManager)
+        {
+            hookManager.InGameHUDInit += HookManager_InGameHUDInit;
+            hookManager.OnEntitySpawn += HookManager_OnEntitySpawn;
+        }
+
+        private void HookManager_InGameHUDInit(Game.GUI.InGameHUD inGameHUD, Game.GUI.Controls.Manager manager)
+        {
+            Faction playerFaction = GnomanEmpire.Instance.World.AIDirector.PlayerFaction;
+            // Iterate over all player entities (gnomes and automata nd whathaveyou)
+            foreach (KeyValuePair<uint, Character> entry in playerFaction.Members)
+            {
+                Character gnome = entry.Value;
+                PatchCharacterBehavior(gnome);
+            }
+        }
+
+        private void HookManager_OnEntitySpawn(Game.GameEntity entity)
+        {
+            if ( entity.GetType() == typeof(Character))
+            {
+                PatchCharacterBehavior((Character)entity);
+            }
+        }
+
+        private void PatchCharacterBehavior(Character gnome)
+        {
+            // Interested only this specific Behavior
+            if (gnome.Behavior.GetType() == typeof(PlayerCharacterBehavior) ||
+                gnome.Behavior.GetType() == typeof(AutomatonBehavior))
+            {
+                FindNextType((System.Collections.IEnumerable)gnome.Behavior, GetTargetTypesForPlayerCharacter().GetEnumerator());
+            }
         }
 
         private void FindNextType(System.Collections.IEnumerable treeNode, IEnumerator<Type> targetList)
