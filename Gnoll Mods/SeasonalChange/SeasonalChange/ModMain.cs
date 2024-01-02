8d7d751485de45b74043c187a2890834ff349c31
DXDoug <32404173+DXDoug@users.noreply.github.com>
2024-01-02 02:56:21 -0600

    Update ModMain.cs

--- Gnoll Mods/SeasonalChange/SeasonalChange/ModMain.cs
+++ Gnoll Mods/SeasonalChange/SeasonalChange/ModMain.cs
@@ -6,6 +6,8 @@ using System.Reflection;
 using System.Text;
 using Game.GUI;
 using Game.GUI.Controls;
+using Game.AudioManager;
+using irrKlang.Net4;
 using GnollModLoader;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -125,5 +127,92 @@ namespace GnollMods.SeasonalChange
             Game.GnomanEmpire.Instance.world_0.Region.Map.TileSet.mTexture = Game.GnomanEmpire.Instance.gameDefs_0.Tilesheet;
         }
 
+    }
+}
+namespace GnollMods.RainSFX
+{
+    class ModMain : IGnollMod
+    {
+        private static readonly string assembly = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        private static readonly string dll = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+        private static readonly string modFolder = Path.Combine(assembly, dll);
+        private static readonly string dataFolder = Path.Combine(modFolder, "Data");
+        private static readonly string rainSoundPath = Path.Combine(modFolder, "RainSFX", "rain03.wav");
+
+        private bool isRaining = false;
+        private ISoundEngine soundEngine;
+
+        public string Name { get { return "RainSFX"; } }
+        public string Description { get { return "Plays rain sound effects when it's raining in the game"; } }
+
+        public string BuiltWithLoaderVersion { get { return "G1.14"; } }
+
+        public int RequireMinPatchVersion { get { return 14; } }
+
+        public void OnEnable(HookManager hookManager)
+        {
+            hookManager.Update += HookManager_Update;
+
+            // Initialize the sound engine
+            soundEngine = new ISoundEngine();
+        }
+
+        public void OnDisable(HookManager hookManager)
+        {
+            hookManager.Update -= HookManager_Update;
+
+            // Release resources when disabling the mod
+            soundEngine.Dispose();
+        }
+
+        public bool IsDefaultEnabled()
+        {
+            return true;
+        }
+
+        public bool NeedsRestartOnToggle()
+        {
+            return false;
+        }
+
+        private void HookManager_Update(double dt)
+        {
+            // Check if it's raining and play/stop the rain sound accordingly
+            if (Game.GnomanEmpire.Instance.Region.Rain && !isRaining)
+            {
+                isRaining = true;
+                PlayRainSound();
+            }
+            else if (!Game.GnomanEmpire.Instance.Region.Rain && isRaining)
+            {
+                isRaining = false;
+                // Stop the rain sound if it's playing
+                StopRainSound();
+            }
+        }
+
+        private void PlayRainSound()
+        {
+            // Check if the rain sound file exists
+            if (File.Exists(rainSoundPath))
+            {
+                Console.WriteLine("-- Playing rain sound");
+
+                // Play the rain sound using irrklang.Net4
+                soundEngine.Play2D(rainSoundPath, false, false);
+            }
+            else
+            {
+                Console.WriteLine("-- Rain sound file not found: " + rainSoundPath);
+            }
+        }
+
+        private void StopRainSound()
+        {
+            Console.WriteLine("-- Stopping rain sound");
+
+            // Stop all sounds
+            soundEngine.StopAllSounds();
+        }
     }
 }
