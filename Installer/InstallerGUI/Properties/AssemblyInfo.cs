f37de370df0160769b7defedf58598c10ea83bc1
Nefaro <xsvenson@gmail.com>
2026-01-24 22:05:49 +0200

    Preparations for v1.15 release

--- Installer/InstallerGUI/Properties/AssemblyInfo.cs
+++ Installer/InstallerGUI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://github.com/Nefaro/gnoll")]
 [assembly: AssemblyProduct("InstallerGUI")]
-[assembly: AssemblyCopyright("Copyright © 2021 - 2025")]
+[assembly: AssemblyCopyright("Copyright © 2021 - 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.14.2.0")]
-[assembly: AssemblyFileVersion("1.14.2.0")]
+[assembly: AssemblyVersion("1.15.0.0")]
+[assembly: AssemblyFileVersion("1.15.0.0")]
